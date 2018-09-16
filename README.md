7643bacb6db0c266b07d8cbf4f4bd2ffc3451197
SergeiSanJj <isara.isara8@gmail.com>
2018-09-16 21:10:24 +0300

    Update Readme

--- README.md
+++ README.md
@@ -6,34 +6,34 @@ To use this bot better choose Linux based OS.
 ## **Install**##
 
 0) Python 3
-		$ sudo apt install python3 
+		 sudo apt install python3 
 
 1) Telegram-cli (from @vysheng) by official guide:
 https://github.com/vysheng/tg/
 
 for UBUNTU:
 ```shell
-$ git clone --recursive https://github.com/vysheng/tg.git && cd tg
+ git clone --recursive https://github.com/vysheng/tg.git && cd tg
 		
-$ sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make 
+ sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make 
 ```  
 
 Go to /loc/to/tg and there:
 ```shell
-$ ./configure && make
+ ./configure && make
 ```  
 if having issues try:
 ```shell
-$ git clone --recursive https://github.com/vysheng/tg.git
-$ cd tg
-$ wget -O openssl.patch 'https://aur.archlinux.org/cgit/aur.git/plain/telegram-cli-git.patch?h=telegram-cli-git'
-$ patch -p1 < openssl.patch
-$ ./configure && make
+ git clone --recursive https://github.com/vysheng/tg.git
+ cd tg
+ wget -O openssl.patch 'https://aur.archlinux.org/cgit/aur.git/plain/telegram-cli-git.patch?h=telegram-cli-git'
+ patch -p1 < openssl.patch
+ ./configure && make
 ```  
 
 try to use telegram-cli by going to /loc/to/tg/bin/ and calling
 ```shell
-$ ./telegram-cli
+ ./telegram-cli
 ```
 if everything works fine you are ready for the next step.
 
@@ -42,27 +42,27 @@ https://github.com/luckydonald/pytg
 
 for UBUNTU:
 ```shell
-$ pip install pytg
-$ git clone https://github.com/luckydonald/pytg.git && cd pytg
+ pip install pytg
+ git clone https://github.com/luckydonald/pytg.git && cd pytg
 ```
 to update:
 ```shell
-$ git pull
-$ sudo python setup.py install
+ git pull
+ sudo python setup.py install
 ```
 
 3) Finally the AutoForest bot
 for UBUNTU:
 ```shell
-$ git clone https://github.com/SergySanJj/AutoForest.git && cd AutoForest
+ git clone https://github.com/SergySanJj/AutoForest.git && cd AutoForest
 
 Open ports for telegram-cli by going to /loc/to/tg/bin/ then
 ```shell
-$ ./telegram-cli --json -P 4458
+ ./telegram-cli --json -P 4458
 ```
 Go to /loc/to/AutoForest/ then:
 ```shell
-$ python3 autoforest.py
+ python3 autoforest.py
 ```
 
 Now AutoForest will run until you Ctrl-Z or shut down terminal.
--- autoforest.py
+++ autoforest.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 __author__ = 'SergySanJj'
 
 """
@@ -6,15 +8,16 @@ Autobot for ChatWars.
 
 from pytg.sender import Sender
 from pytg.receiver import Receiver
-
+from pytg.utils import coroutine
 
 import time
 import datetime
 import logging
 import math
+import json
 
 #logs everything connected with tg-cli into console
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 #path to telegram-cli dir:
 tg_cli_dir = "/loc/to/tg/"
@@ -28,16 +31,24 @@ debug_bot = "@ChatGrinding_bot"
 #./telegram-cli --json -P 4458
 #after what port 4458 will be opened
 
-receiver = Receiver(host="localhost", port=4458)
-sender = Sender(host="localhost", port=4458)
+
 
 activities_ru = {
 	'forest': "🌲Лес",
-	'defend': "🛡Защита"}
+	'defend': "🛡Защита",
+	'fight' : "▶️Быстрый бой",
+	'hero'  : "🏅Герой"}
 
 activities_eu = {
 	'forest': "🌲Forest",
-	'defend': "🛡Defend"}
+	'defend': "🛡Defend",
+	'fight' : "▶️Fast fight"}
+
+states_ru = {
+	'rest': "Состояние:\n🛌Отдых"}
+
+message_types_ru = {
+	'main': "Уровень"}
 
 battle_table_ru = [
 	datetime.datetime.strptime("01:00:00", "%H:%M:%S"),
@@ -49,14 +60,17 @@ battle_table_eu = [
 	datetime.datetime.strptime("10:00:00", "%H:%M:%S"),
 	datetime.datetime.strptime("18:00:00", "%H:%M:%S")]
 
+
 #returns time difference in seconds
 def diff(start, end):
 	start_sec = start.hour*3600 + start.minute*60 + start.second
 	end_sec = end.hour*3600 + end.minute*60 + end.second
 	return (end_sec - start_sec)
 
+
 #returns True if it's time to defend
-def defend_check(input_time):
+def defend_check():
+	curr_time = datetime.datetime.now()
 	for t in battle_table_ru:
 		differ = diff(input_time,t)
 		#print(differ)
@@ -64,15 +78,102 @@ def defend_check(input_time):
 			return True
 	return False
 
-def main():	
-	message = "лу"
+
+def state_check(receiver, sender):
+	receiver.start()
+	message = activities_ru['hero']
+	sender.send_msg(ru_bot, message)
+	
+	receiver.message(message_handler(sender))
+
+
+@coroutine 
+def state_handler(sender):
+	quit = False
+	try:
+		while not quit:  # loop for messages
+			msg = (yield)  
+			sender.status_online()  # so we will stay online.
+
+			
+			if msg.event != "message":
+				continue  # is not a message.
+			if msg.text is None:  # we have media instead.
+				continue  
+
+			if msg.text is not None:
+				if msg.sender.username ==  'ChatWarsBot':
+					print(msg['text'])
+					if message_types_ru['main'] in msg['text']:
+						print("Main message:")
+						if states_ru['rest'] in msg['text']:
+							print("Resting")
+							return True
+						else:
+							print("Busy")
+							return False
+
+	except GeneratorExit:
+		# the generator (pytg) exited (got a KeyboardIterrupt).
+        	pass
+	except KeyboardInterrupt:
+		# we got a KeyboardIterrupt(Ctrl+C/Ctrl+Z)
+		pass
+	else:
+		# the loop exited without exception, becaues _quit was set True
+		pass
+	print("Busy")
+	return False	
+
+
+@coroutine
+def message_handler(sender): 
+	quit = False
+	try:
+		while not quit:  # loop for messages
+			msg = (yield)  
+			sender.status_online()  # so we will stay online.
+
+			
+			if msg.event != "message":
+				continue  # is not a message.
+			if msg.text is None:  # we have media instead.
+				continue  
+
+			if msg.text is not None:
+				if msg.sender.username ==  'ChatWarsBot':
+					print(msg['text'])
+
+	except GeneratorExit:
+		# the generator (pytg) exited (got a KeyboardIterrupt).
+        	pass
+	except KeyboardInterrupt:
+		# we got a KeyboardIterrupt(Ctrl+C/Ctrl+Z)
+		pass
+	else:
+		# the loop exited without exception, becaues _quit was set True
+		pass
+	print("no new messages")
+
+
+def main_cycle(receiver, sender):
 	while True:
-		sender.send_msg(debug_bot, message)
-		message += "у"
-		curr_time = datetime.datetime.now()
+		if defend_check():
+			
+			print("Time to defend")
 		
-		print("Time to defend:", defend_check(curr_time))
+		receiver.start()
+
+		receiver.message(message_handler(sender))
+
+		receiver.stop()
 		time.sleep(5)
-		
+
+
+def main():	
+	receiver = Receiver(host="localhost", port=4458)
+	sender = Sender(host="localhost", port=4458)
+	main_cycle(receiver, sender)
+
 
 main()
